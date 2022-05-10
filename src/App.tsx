a3b689c73be8996d941f69af9b3be6812a2c4a02
BaturaDima <mr.koonnot@mail.ru>
2022-05-10 10:04:43 +0300

    Front part for creating orders,pending, accept orders

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './App.css';
+import { IndexRoutes } from './routes';
+
+export const App: React.FC = () => <IndexRoutes />;
---
+++ src/pages/users/managemnt/models/driver-application.interface.ts
@@ -0,0 +1,7 @@
+export interface DriverApplication {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  isApplicationApproved: boolean;
+}
